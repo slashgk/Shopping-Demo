52aac245d35b16e4992761aa46a4561245b7d1ce
Gaurav <gaurav.grd@gmail.com>
2019-01-30 12:06:35 +0530

    testing 1

--- server/server.js
+++ server/server.js
@@ -14,11 +14,7 @@ app.get('/',(req, res) => {
     res.sendfile('client/login.html');
 });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.post('/users', (req, res) => {
     var user = new User({
         username: req.query.username,
